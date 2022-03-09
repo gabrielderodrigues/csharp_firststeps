6994d8b5c82702d8bb353dd63059face5bf58bb8
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-03-09 10:30:25 -0300

    Entendendo melhor For e While utilizando exemplos como fatorial e for encadeado.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto Fatorial");
+
+            int fatorial = 1;
+
+            for (int i = 1; i < 11; i++)
+            {
+                fatorial *= 1;
+                Console.WriteLine("Fatorial de " + i + " = " + fatorial);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiplosDeTres/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplosDeTres
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto Multiplo de Três");
+
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
